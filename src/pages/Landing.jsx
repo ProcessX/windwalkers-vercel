ad4b91fbe5241045d0bf1d8abaef624dfdc752cd
ProcessX <luc2526@gmail.com>
2020-05-19 15:34:40 +0200

    Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Btn from "../components/Btn";
 import {Redirect} from "react-router-dom";
 
+
 class Landing extends Component {
 
   constructor() {
@@ -17,13 +18,44 @@ class Landing extends Component {
     this.setState({redirectURL});
   }
 
+  relocateTo = (url) => {
+    //window.location.href = url;
+    window.open(url, '_blank');
+  }
+
   render() {
+    const baseURL = process.env.PUBLIC_URL + '/assets/';
+
+    const introStyle = {
+      backgroundImage: `url(${baseURL}Landing-Illu.png)`,
+    }
+
+    const mainTitleStyle = {
+      backgroundImage: `url(${baseURL}Title-Illu.png)`,
+    }
+
+    const conceptStyle01 = {
+      backgroundImage: `url(${baseURL}Wind-Icon.png)`,
+    }
+
+    const conceptStyle02 = {
+      backgroundImage: `url(${baseURL}Horde-Icon.png)`,
+    }
+
+    const artworkStyle01 = {
+      backgroundImage: `url(${baseURL}OregonTrail-Icon.png)`,
+    }
+
+    const artworkStyle02 = {
+      backgroundImage: `url(${baseURL}Book-Icon.png)`,
+    }
+
     const {redirectURL} = this.state;
     return (
       <div className={'page page--landing'}>
-        <section className={'landing__section landing__section--intro'}>
+        <section className={'landing__section landing__section--intro'} style={introStyle}>
           <div className={'landing__content'}>
-            <h1 className={'title title--landing'}>Windwalkers - a fan's game</h1>
+            <h1 className={'title title--landing'} style={mainTitleStyle}>Windwalkers</h1>
             <p className={'content__intro'}>Inspirée du jeu Oregon Trail et du roman de SF La Horde du Contrevent.</p>
             <Btn action={() => this.redirectTo('/game/')} title={'Tester la démo'}/>
           </div>
@@ -34,12 +66,12 @@ class Landing extends Component {
             <h2 className={'title title--landing--section title--landing--section--lightBlue'}>Explorer et découvrir</h2>
             <ul className={'concept__li'}>
               <li className={'concept__el'}>
-                <div className={'concept__illu'}>illu</div>
+                <div className={'concept__illu'} style={conceptStyle01}>illu</div>
                 <p className={'concept__description'}>Affrontez des bourrasques incessantes, capables de déraciner des arbres et d’abattre des villages.</p>
               </li>
               <li className={'concept__el'}>
-                <div className={'concept__illu'}>illu</div>
-                <p className={'concept__description'}>Affrontez des bourrasques incessantes, capables de déraciner des arbres et d’abattre des villages.</p>
+                <div className={'concept__illu'} style={conceptStyle02}>illu</div>
+                <p className={'concept__description'}>Menez la 34ème Horde du Contrevent, les plus tenaces des explorateurs. Soyez le premier à atteindre la source du vent, où tout s’arrête et tout commence.</p>
               </li>
             </ul>
           </div>
@@ -51,18 +83,18 @@ class Landing extends Component {
             <p className={'context'}>Le projet scolaire Windwalkers est venu au monde de la volonté de mélanger deux œuvres passionnantes :</p>
             <ul className={'artwork__li'}>
               <li className={'artwork__el'}>
-                <div className={'artwork__illu'}>illu</div>
+                <div className={'artwork__illu'} style={artworkStyle01}>illu</div>
                 <h3 className={'artwork__title'}>Oregon Trail</h3>
-                <h4 className={'artwork__info'}>(Apple II, 1985)</h4>
+                <h4 className={'artwork__info'}>(Apple II, 1990)</h4>
                 <p className={'artwork__description'}>Célèbre jeu de gestion et d’exploration de l’époque micro.</p>
-                <Btn title={'Découvrir'}/>
+                <Btn title={'Découvrir'} action={() => this.relocateTo('https://www.myabandonware.com/game/the-oregon-trail-2ku')}/>
               </li>
               <li className={'artwork__el'}>
-                <div className={'artwork__illu'}>illu</div>
+                <div className={'artwork__illu'} style={artworkStyle02}>illu</div>
                 <h3 className={'artwork__title'}>La Horde du Contrevent</h3>
                 <h4 className={'artwork__info'}>(Alain Damasio, 2003)</h4>
                 <p className={'artwork__description'}>Roman de SF français au succès titanesque.</p>
-                <Btn title={'Découvrir'}/>
+                <Btn title={'Découvrir'} action={() => this.relocateTo('https://lavolte.net/livres/la-horde-du-contrevent/')}/>
               </li>
             </ul>
           </div>

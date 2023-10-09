869d3ed712fb670fff78ede71a11816a145a5ab4
ProcessX <luc2526@gmail.com>
2023-10-09 11:55:27 +0200

    change heroku stack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,91 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Btn from "../components/Btn";
 import HomeScene from "../components/HomeScene";
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 class Home extends Component {
-
   constructor() {
     super();
     this.state = {
       redirectURL: null,
       titleAnimation: false,
-    }
+    };
   }
-
 
   componentDidMount() {
-    this.props.playMusic('home');
+    this.props.playMusic("home");
   }
-
 
   redirectTo = (url) => {
-    let {redirectURL} = this.state;
+    let { redirectURL } = this.state;
     redirectURL = url;
-    this.setState({redirectURL});
-  }
-
+    this.setState({ redirectURL });
+  };
 
   startTitleAnimation = () => {
-    let {titleAnimation} = this.state;
+    let { titleAnimation } = this.state;
     titleAnimation = true;
-    this.setState({titleAnimation});
-  }
-
+    this.setState({ titleAnimation });
+  };
 
   render() {
-    const {redirectURL, titleAnimation} = this.state;
+    const { redirectURL, titleAnimation } = this.state;
 
-
-    let pageClass = titleAnimation ? 'page--home--hideNav' : '';
-
-
+    let pageClass = titleAnimation ? "page--home--hideNav" : "";
 
     return (
       <div className={`page page--home ${pageClass}`}>
-
         <HomeScene
           titleAnimation={titleAnimation}
-          startGame={() => this.redirectTo('/game/narration/')}
+          startGame={() => this.redirectTo("/game/narration/")}
         />
 
-        <h1 className={'home__title illu pixelArt'}>Windwalkers</h1>
+        <h1 className={"home__title illu pixelArt"}>Windwalkers</h1>
 
-        <nav className={'menu menu--fullScreen menu--home'}>
-          <ul className={'menu__btn__li'}>
-            <li className={'menu__btn__el'}>
+        <nav className={"menu menu--fullScreen menu--home"}>
+          <ul className={"menu__btn__li"}>
+            <li className={"menu__btn__el"}>
+              <Btn title={"Jouer"} action={() => this.startTitleAnimation()} />
+            </li>
+
+            <li className={"menu__btn__el"}>
               <Btn
-                title={'Jouer'}
-                action={() => this.startTitleAnimation()}
+                title={"Options"}
+                action={() => this.redirectTo("/game/options")}
               />
             </li>
 
-            <li className={'menu__btn__el'}>
+            <li className={"menu__btn__el"}>
               <Btn
-                title={'Options'}
-                action={() => this.redirectTo('/game/options')}
-              />
-            </li>
-
-            <li className={'menu__btn__el'}>
-              <Btn
-                title={'Crédits'}
-                action={() => this.redirectTo('/game/credits')}
+                title={"Crédits"}
+                action={() => this.redirectTo("/game/credits")}
               />
             </li>
           </ul>
         </nav>
 
-        <ul className={'home__info__li'}>
-          <li className={'home__info__el'}>
-            <p className={'home__info'}>2020 - Lucas Geshef</p>
+        <ul className={"home__info__li"}>
+          <li className={"home__info__el"}>
+            <p className={"home__info"}>2020 - Lucas Geshef </p>
           </li>
-          <li className={'home__info__el'}>
-            <p className={'home__info'}>Albert Jacquard (DWM)</p>
+          <li className={"home__info__el"}>
+            <p className={"home__info"}>Albert Jacquard (DWM)</p>
           </li>
         </ul>
 
-        {redirectURL ? <Redirect to={redirectURL}/> : null}
+        {redirectURL ? <Redirect to={redirectURL} /> : null}
       </div>
     );
   }
